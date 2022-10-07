32ac79fe8fb19cdbd8396ae9857b0753d6af6184
“QDSAD” <2747209319@qq.com>
2022-10-07 21:01:18 +0800

    non update

--- MySTL/MAIN.cpp
+++ MySTL/MAIN.cpp
@@ -12,10 +12,11 @@ after_and_befor aab;
 //#include "ListTest.h"	//test is ok
 //#include "DequeTest.h"//已实现功能可正确运行，但会发生重复链接问题未解决，只能将DequeTest.h代码
 //移动到MAIN中运行
-#include "StackTest.h"
+//#include "StackTest.h" //test is ok
+//#include "QueueTest.h" //test is ok
 
 //  /FORCE:MULTIPLE:不管是否重复定义
 signed main() {
-	MySTL::StackTest::testAllCases();
+	//MySTL::QueueTest::testAllCases();
 	//testAllCases();
 }
