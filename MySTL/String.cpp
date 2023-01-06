568ad6636206e91ddcfc64e1462dcec1a39ab5ce
company <2747209319@qq.com>
2023-01-06 17:06:16 +0800

    add string

---
+++ MySTL/String.cpp
@@ -0,0 +1,146 @@
+#include "String"
+#include "String.h"
+
+using namespace MySTRING;
+
+using iterator = typename string::iterator;
+using const_iterator = typename string::const_iterator;
+using size_type = typename string::size_type;
+using reference = typename string::reference;
+using const_reference = typename string::const_reference;
+
+string::string():_start(0),_finsh(0),_end_of_storage(0) {
+
+}
+
+string::string(const string& str) {
+
+	
+}
+
+void string::deallocate() {
+
+	dataAllocator::deallocate(_start, capacity());
+}
+
+void MySTRING::string::allocate(size_type sz, value_type c){
+
+	_start = dataAllocator::allocate(sz);
+	_finsh = MySTL::uninitialized_fill_n(_start, sz, c);
+	_end_of_storage = _finsh;
+}
+
+iterator MySTRING::string::begin(){
+
+	return _start;
+}
+
+const_iterator MySTRING::string::cbegin() const {
+	
+	return _finsh;
+}
+
+iterator MySTRING::string::end() {
+
+	return _finsh;
+}
+
+const_iterator MySTRING::string::cend() const {
+
+	return _finsh;
+}
+
+size_type MySTRING::string::size() const{
+
+	return _finsh - _start;
+}
+
+size_type MySTRING::string::length() const{
+
+	return _finsh - _start;
+}
+
+size_type MySTRING::string::capacity() const {
+
+	return _end_of_storage - _start;
+}
+
+void MySTRING::string::resize(size_type sz, char c) {
+
+	if (sz < size()) {
+		_finsh = _finsh - (size() - sz);
+	}
+	else if (sz < capacity()) {
+		
+		_finsh = MySTL::uninitialized_fill_n(_finsh, sz, c);
+	}
+	else {
+		size_type oldCap = capacity();
+		size_type newCap = (oldCap == 0) ? sz : (oldCap + sz);
+
+		deallocate();
+		
+		_start = dataAllocator::allocate(newCap);
+		_finsh = MySTL::uninitialized_fill_n(_start, sz, c);
+		_end_of_storage = _start + newCap;
+	}
+}
+
+void MySTRING::string::reserver(size_type sz) {
+
+	if (sz <= capacity()) {
+		return;
+	}
+
+	iterator newStart = dataAllocator::allocate(sz);
+	iterator newFinsh = MySTL::uninitialized_copy(_start, _finsh, newStart);
+	iterator newEnd = newStart + sz;
+
+	deallocate();
+
+	_start = newStart;
+	_finsh = newFinsh;
+	_end_of_storage = newEnd;
+}
+
+void MySTRING::string::clear() {
+
+	_finsh = _start;
+}
+
+bool MySTRING::string::empty(){
+
+	return (size() == 0) ? true : false;
+}
+
+void MySTRING::string::shrink_to_fit() {
+
+	size_type oldSize = size();
+	iterator newStart = dataAllocator::allocate(oldSize);
+	iterator newFinsh = MySTL::uninitialized_copy(_start, _finsh, newStart);
+	iterator newEnd = newFinsh;
+
+	deallocate();
+
+	_start = newStart;
+	_finsh = newFinsh;
+	_end_of_storage = newEnd;
+}
+
+reference MySTRING::string::operator[](size_type pos) {
+
+	if (pos >= size()) {
+		throw("error: string::operator[] over range\n");
+	}
+	
+	return *(_start + pos);
+}
+
+const_reference MySTRING::string::operator[](size_type pos) const {
+	
+	if (pos >= size()) {
+		throw("error: string::operator[] over range\n");
+	}
+
+	return *(_start + pos);
+}
--- MySTL/String.h
+++ MySTL/String.h
@@ -1,9 +1,74 @@
 #ifndef _STRING_H_2023_1_6_
 #define _STRING_H_2023_1_6_
 
-namespace STRING {
+#include <stddef.h>
 
-	
+#include "Allocator.h"
+#include "Utlity.h"
+
+namespace MySTRING {
+
+	class string {
+	public:
+		
+		typedef char		value_type;
+
+		typedef char*		pointer;
+		typedef char*		iterator;
+		typedef char&		reference;
+		typedef const char* const_pointer;
+		typedef const char* const_iterator;
+		typedef const char& const_reference;
+
+		typedef size_t		size_type;
+		typedef ptrdiff_t	difference_type;
+
+		typedef MySTL::allocator<char> dataAllocator;
+		
+		//construct
+		string();
+		string(const string& str);
+		string(const string& str, size_t pos, size_t len);
+		string(const char* s);
+		string(const char* s, size_t n);
+		string(size_t n, char c);
+		template <class InputIterator>  
+		string(InputIterator first, InputIterator last);
+
+		virtual ~string();
+
+		//Iterators
+		iterator begin();
+		const_iterator cbegin() const;
+
+		iterator end();
+		const_iterator cend() const;
+
+		//Capacity
+		size_type size() const;
+		size_type length() const;
+		size_type capacity()const;
+		void resize(size_type sz, char c = 0);
+		void reserver(size_type sz);//改变容量
+		void clear();
+		bool empty();
+		void shrink_to_fit();//去掉预留空间
+
+		reference operator[](size_type pos);
+		const_reference operator[](size_type pos) const;
+
+
+
+	private:
+		iterator _start;
+		iterator _finsh;
+		iterator _end_of_storage;
+
+		void deallocate();
+		void allocate(size_type sz, value_type c = 0);
+		
+
+	};
 }
 
 #endif // !_STRING_H_2023_1_6_
